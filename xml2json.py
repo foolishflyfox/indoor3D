45a70afc6cf2b074206c0e83364492d5935de571
foolishflyfox <fenghuabin1992@163.com>
2020-02-25 17:36:39 +0800

    add some function declaration in xml2json.py that implement tonight

--- xml2json.py
+++ xml2json.py
@@ -4,27 +4,48 @@ import json
 import random
 from flask import render_template
 
+# TODO: 将一个 subroom 解析成一个 FuncArea 对象
+# subroom 是一个 xml.dom.minidom.Element 类型
+def parseSubroom(subroom):
+    FuncArea = {'FuncArea': True}
+    return FuncArea
+
+# TODO: 构建 Floor 对象
+def CreateFloor(FuncAreas):
+    Floor = {"_id":1, "Name":"F1", "High":5, "FuncAreas":[],
+        "PubPoint":[]}
+    for FuncArea in FuncAreas:
+        Floor['FuncAreas'].append(FuncArea)
+    return Floor
+
+# TODO: 构建 Building 对象
+def CreateBuilding(Floors):
+    building = {'building': True}
+    return building
 
 def CreateMapJsonFile(geoxml_path, geojson_path):
     result = {'data':{'Floors':[]}}
-    floors = result['data']['Floors']
+    Floors = result['data']['Floors']
+
     dom = xml.dom.minidom.parse(geoxml_path)
     geometry = dom.documentElement
+    # 一个 map 只能有一个rooms
     rooms = geometry.getElementsByTagName('rooms')[0]
     room_list = rooms.getElementsByTagName('room')
-    floor = {"_id":1, "Name":"F1", "High":5, "FuncAreas":[],
-        "PubPoint":[]}
-    FuncAreas = floor['FuncAreas']
+    
+    FuncAreas = []
     for room in room_list:
         subrooms = room.getElementsByTagName('subroom')
         for subroom in subrooms:
-            FuncArea = {'Category': random.randint(101, 109)}
+            FuncArea = parseSubroom(subroom)
             FuncAreas.append(FuncArea)
-            FuncArea['Outline'] = [[[]]]
-            Outline = FuncArea['Outline'][0][0]
 
-    floors.append(floor)
-    print(json.dumps(result))
+    Floor = CreateFloor(FuncAreas)
+    Floors.append(Floor)
+    result['data']['building'] = CreateBuilding(Floors)
+    # print(json.dumps(result, indent=2))
+    with open(geojson_path, 'w') as output:
+        json.dump(result, output, indent=2)
     
 
 def map_xml2json(simname):
